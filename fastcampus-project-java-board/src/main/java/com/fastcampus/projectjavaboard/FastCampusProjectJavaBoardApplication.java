dbd971f0d3c7574f426da2e1b96807248a432f81
victoryhwan0421 <victoryhwan@naver.com>
2024-01-23 12:45:31 +0900

    #11 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간 초기 기술:
    -  스프링 웹
    - 액츄에이터
    - 데브툴즈
    - 롬복

---
+++ fastcampus-project-java-board/src/main/java/com/fastcampus/projectjavaboard/FastCampusProjectJavaBoardApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectjavaboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCampusProjectJavaBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCampusProjectJavaBoardApplication.class, args);
+    }
+
+}
