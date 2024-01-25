05f116b8e355a784fb64e43f34be3c091f477765
victoryhwan0421 <victoryhwan@naver.com>
2024-01-25 15:37:53 +0900

    #3 - Spring Data JPA 속성과 설정
    - jpa auditing 기능을 쓰기 위해 'jpaConfig' 추가
    - db 접근과 개발 편의를 위한 각종 부트 프로퍼티 적용
    - TODO: 패스워드를 프로퍼티에 노출시켰다. 처리 필요

---
+++ fastcampus-project-java-board/src/main/java/com/fastcampus/projectjavaboard/config/JpaConfig.java
@@ -0,0 +1,20 @@
+package com.fastcampus.projectjavaboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        // TODO: 스프링 시큐리티로 인증 기능을 붙이게 될 때(누가 인증하는지 식별하게 될 때), 수정하자.
+        // 현재는 createdBy, modifiedBy 모두 aae123 으로 들어올 것
+        return () -> Optional.of("aaaa");
+    }
+}
