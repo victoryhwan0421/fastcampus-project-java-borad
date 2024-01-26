dab4447d25928edfdf3e79e85d4589db38850409
victoryhwan0421 <victoryhwan@naver.com>
2024-01-26 13:52:10 +0900

    #1 - 기타 수정사항
    기타 사항 수정

--- fastcampus-project-java-board/src/main/java/com/fastcampus/projectjavaboard/config/JpaConfig.java
+++ fastcampus-project-java-board/src/main/java/com/fastcampus/projectjavaboard/config/JpaConfig.java
@@ -15,6 +15,6 @@ public class JpaConfig {
     public AuditorAware<String> auditorAware() {
         // TODO: 스프링 시큐리티로 인증 기능을 붙이게 될 때(누가 인증하는지 식별하게 될 때), 수정하자.
         // 현재는 createdBy, modifiedBy 모두 aae123 으로 들어올 것
-        return () -> Optional.of("aaaa");
+        return () -> Optional.of("aae1");
     }
 }
