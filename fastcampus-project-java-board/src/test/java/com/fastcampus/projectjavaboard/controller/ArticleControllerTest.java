73dd89bdd403579f13359644fdca51492af604a5
victoryhwan0421 <victoryhwan@naver.com>
2024-01-26 18:02:31 +0900

    #5 - 게시글 관련 view 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.
    
    기존 테스트를 실패한 채 올리면 ₩gradle.build₩가 실패하여 배포 자동화에 악영향을 미치는 경우가 있으므로, 실패 테스트는 테스트 제외 처리하는 방향으로 정책 결정
    이에 기능 구현 중인 유닛 테스트를 모두 ₩@Disabled₩ 처리
    
    그 밖에 추가로 생각난 테스트 방법을 추가

---
+++ fastcampus-project-java-board/src/test/java/com/fastcampus/projectjavaboard/controller/ArticleControllerTest.java
@@ -0,0 +1,99 @@
+package com.fastcampus.projectjavaboard.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+    private final MockMvc mvc;
+
+    public ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+
+    /**
+     * API 경로: /articles
+     * @throws Exception
+     */
+    @Disabled("구현 중")   // 실패하는 테스트는 커밋하지 않는 것을 원칙으로 하기 위한 애노테이션
+    @DisplayName("[view] [GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticlesView_thenReturningArticlesView() throws Exception {
+        // Given
+
+
+        // When_Then
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"));
+    }
+
+
+    /**
+     * API 경로: /articles
+     */
+    @Disabled("구현 중")
+    @DisplayName("[view] [GET] 게시글 상세 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleView_thenReturningArticleView() throws Exception {
+        // Given
+
+
+        // When_Then
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("article"))
+                .andExpect(model().attributeExists("articleComments"));
+    }
+
+
+    /**
+     * API 경로: /articles/search
+     */
+    @Disabled("구현 중")
+    @DisplayName("[view] [GET] 게시글 검색 전용 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleSearchView_thenReturningArticleSearchView() throws Exception {
+        // Given
+
+
+        // When_Then
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search"));
+    }
+
+
+    /**
+     * API 경로: /articles/search-hashtag
+     */
+    @Disabled("구현 중")
+    @DisplayName("[view] [GET] 게시글 해시태그 검색 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenRequestingArticleHashtagSearchView_thenReturningArticleHashtagSearchView() throws Exception {
+        // Given
+
+
+        // When_Then
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/search-hashtag"));
+    }
+}
